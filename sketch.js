4db4187d42361142baa8a31d8d420486eb28cd07
rhea1237 <92313380+rhea1237@users.noreply.github.com>
2021-10-28 18:16:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+var S1 
+var S2
+
+function setup() {
+  createCanvas(400,400);
+  S1 = new Student(10, 5, "chloe")
+  S1.display()
+
+}
+
+function draw() 
+{
+  background(30);
+
+  
+}
---
+++ student.js
@@ -0,0 +1,17 @@
+
+class Student
+{
+   constructor(age, grade, name){
+       this.age = age;
+       this.grade = grade;
+       this.name = name;
+   }
+   display(){
+       console.log(this.name)
+       console.log(this.age)
+       console.log(this.grade)
+   }
+}
+
+
+
